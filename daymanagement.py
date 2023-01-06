7229b2fba2dc78cc304bffd965e74718a43d8c76
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-06 15:44:15 +0300

    Added a heading for my day tasks

--- daymanagement.py
+++ daymanagement.py
@@ -1,17 +1,21 @@
 from datetime import datetime
 import requests
+import json
 
 my_day = "My day's Things To Do: "
 print(my_day.center(70))
+def jprint(obj):
+    text = json.dumps(obj, sort_keys =True, indent=4)
+    print(text)
 response = requests.get("https://type.fit/api/quotes")
-print(response.status_code)
+print(response.json)
 accomplishes = []
 today = datetime.now()
 whole_today = today.strftime("%A-%d-%m-%Y %H:%M:%S")
 while True:
         Do_the_things = input("Things to accomplish: ")
         if Do_the_things == '':
-         print(f"{whole_today}: That's it for today!")
+         print(f"{whole_today}: {accomplishes} That's it for today!")
          break
         accomplishes.append(Do_the_things)
         print(accomplishes)
