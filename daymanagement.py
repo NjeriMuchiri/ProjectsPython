528bd57290ff7750b773bedc2e8558e272bd38f6
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-06 15:10:47 +0300

    Just learnt something with parameters

--- daymanagement.py
+++ daymanagement.py
@@ -3,7 +3,7 @@ my_day = "My day's Things To Do: "
 print(my_day.center(70))
 accomplishes = []
 today = datetime.now()
-whole_today = today.strftime("%A-%d-%m-%Y %H: %M: %S")
+whole_today = today.strftime("%A-%d-%m-%Y %H:%M:%S")
 while True:
         Do_the_things = input("Things to accomplish: ")
         if Do_the_things == '':
