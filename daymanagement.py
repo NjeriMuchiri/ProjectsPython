33b9ca5133e91a07f3ba8d1f34c7cda4f5e76658
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-06 12:48:47 +0300

    Using class methods to create my day structure project

--- daymanagement.py
+++ daymanagement.py
@@ -1,22 +1,12 @@
-# my_day = "My day's Things To Do: "
-# print(my_day.center(70))
-# accomplishes = []
-# while True:
-#         Do_the_things = input("Things to accomplish: ")
-#         if Do_the_things == 'done':
-#          break
-#         accomplishes.append(Do_the_things)
-#         print(accomplishes)
+my_day = "My day's Things To Do: "
+print(my_day.center(70))
+accomplishes = []
+while True:
+        Do_the_things = input("Things to accomplish: ")
+        if Do_the_things == 'done':
+         break
+        accomplishes.append(Do_the_things)
+        print(accomplishes)
 
-
-
-class DayStructure:
-    def __init__(self, date,day,accomplishes):
-        self.date = date
-        self.day = day
-        self.accomplishes = accomplishes
     
-    
-    
-     
 
