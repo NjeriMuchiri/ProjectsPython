702efc108f0983132dcf45dcd683d8dacfac449f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-06 10:23:53 +0300

    Making my first project of how to manage my day

---
+++ daymanagement.py
@@ -0,0 +1,9 @@
+my_day = "My day's Things To Do: "
+print(my_day.center(70))
+accomplishes = []
+while True:
+        Do_the_things = input("Things to accomplish: ")
+        accomplishes.append(Do_the_things)
+        print(accomplishes)
+        
+
