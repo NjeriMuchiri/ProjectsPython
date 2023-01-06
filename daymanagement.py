c977658eda08dad755f428a35a9c8b3731d70411
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-06 15:03:14 +0300

    Added a new print to make my programme interesting

--- daymanagement.py
+++ daymanagement.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 my_day = "My day's Things To Do: "
 print(my_day.center(70))
 accomplishes = []
+today = datetime.now()
+whole_today = today.strftime("%A-%d-%m-%Y %H: %M: %S")
 while True:
         Do_the_things = input("Things to accomplish: ")
-        if Do_the_things == 'done':
+        if Do_the_things == '':
+         print(f"{whole_today} That's it for today!")
          break
         accomplishes.append(Do_the_things)
         print(accomplishes)
