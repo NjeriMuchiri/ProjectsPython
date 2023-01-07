7ee237b904efc14d7397814b65d8eda1a9833046
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-07 15:02:31 +0300

    Fetching the quotes api

--- daymanagement.py
+++ daymanagement.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 import requests
-import json
+import random
 
 my_day = "My day's Things To Do: "
 print(my_day.center(70))
-def jprint(obj):
-    text = json.dumps(obj, sort_keys =True, indent=4)
-    print(text)
-response = requests.get("https://type.fit/api/quotes")
-print(response.json)
+
+quotes_api = "https://type.fit/api/quotes"
+response = requests.get(quotes_api)
+# print(response.status_code)
+# print(response.json)
+quote = response.json()[0]
+print(quote)
 accomplishes = []
 today = datetime.now()
 whole_today = today.strftime("%A-%d-%m-%Y %H:%M:%S")
+print(whole_today)
 while True:
         Do_the_things = input("Things to accomplish: ")
         if Do_the_things == '':
